e9071e57b1722324fcdd4e69022697f33688b7b3
Ilya Boyandin <ilya@interactivethings.com>
2014-10-21 15:46:21 +0200

    bug moves

--- src/reagent_workshop.cljs
+++ src/reagent_workshop.cljs
@@ -7,18 +7,19 @@
 
 ; Constants
 
-(def field-size 25)
-(def num-cells (* field-size field-size))
-(def cell-size 20)
+(def FIELD-SIZE 25)
+(def NUM-CELLS (* FIELD-SIZE FIELD-SIZE))
+(def CELL-SIZE 20)
+(def KEYS {37 :left 38 :up 39 :right 40 :down})
 
 
 ; Helpers
 
 (defn pos-x [ci]
-  (* (rem ci field-size) cell-size))
+  (* (rem ci FIELD-SIZE) CELL-SIZE))
 
 (defn pos-y [ci]
-  (* (quot ci field-size) cell-size))
+  (* (quot ci FIELD-SIZE) CELL-SIZE))
 
 
 
@@ -26,43 +27,49 @@
 
 (def app-state
   (reagent/atom {
-      :bug (quot num-cells 2)
+      :bug (quot NUM-CELLS 2)
       :fruit nil }))
 
 
 (defn spawn-new-fruit! []
   (swap! app-state
-         assoc :fruit (rand-int num-cells))
-
-  (println (clj->js @app-state)))
+         assoc :fruit (rand-int NUM-CELLS)))
 
 
+(defn move-pos [direction pos]
+  (+ pos (direction {:up   (- FIELD-SIZE)
+                    :down  FIELD-SIZE
+                    :left  -1
+                    :right +1})))
 
+(defn move-bug! [direction]
+  (swap! app-state
+         assoc :bug (move-pos direction (:bug @app-state))))
 
 
 ; Views
 
 (defn fruit [ci]
-  (let [m  (/ cell-size 2)
+  (let [m  (/ CELL-SIZE 2)
         cx (+ (pos-x ci) m)
         cy (+ (pos-y ci) m)]
     [:circle
       {:className "fruit"
-       :width     cell-size
-       :height    cell-size
+       :width     CELL-SIZE
+       :height    CELL-SIZE
        :cx        cx
        :cy        cy
        :r         (dec m)}]))
 
 
 (defn bug [ci]
-  (let [m  (/ cell-size 2)
+  (let [m  (/ CELL-SIZE 2)
         cx (+ (pos-x ci) m)
         cy (+ (pos-y ci) m)]
     [:circle
       {:className "bug"
-       :width     cell-size
-       :height    cell-size
+       :width     CELL-SIZE
+       :height    CELL-SIZE
        :cx        cx
        :cy        cy
        :r         (- m 3)
@@ -75,8 +82,8 @@
 
    [:rect {
       :className "cell-rect"
-      :width     cell-size
-      :height    cell-size
+      :width     CELL-SIZE
+      :height    CELL-SIZE
       :x         (pos-x ci)
       :y         (pos-y ci)} ]
 
@@ -87,24 +94,28 @@
 
 (defn field [bug fruit]
   [:svg {:className "field"
-         :width (* field-size cell-size)
-         :height (* field-size cell-size)}
+         :width (* FIELD-SIZE CELL-SIZE)
+         :height (* FIELD-SIZE CELL-SIZE)}
 
-   (for [ci (range (* field-size field-size))]
+   (for [ci (range (* FIELD-SIZE FIELD-SIZE))]
       ^{:key ci}
        (cell ci
-             :has-bug (= ci bug)
-             :has-fruit (= ci fruit)))])
+             :has-fruit (= ci fruit)
+             :has-bug (= ci bug) ))])
 
 
 (defn main-view []
-  [:div {:className "app"}
+  [:div {:className "app" }
    [field
     (:bug @app-state)
     (:fruit @app-state)]])
 
 
+(defn on-key-down [evt]
+  (if-let [key (KEYS (.-keyCode evt))] (move-bug! key)))
+
+
 (defn ^:export run []
+  (reagent/render-component [main-view] (.-body js/document))
   (spawn-new-fruit!)
-  (reagent/render-component [main-view]
-                              (.-body js/document)))
+  (js/addEventListener "keydown" on-key-down))
