34378e47197d619c33bcb9bfd698f572ca43e40d
Ilya Boyandin <ilya@interactivethings.com>
2014-10-21 15:18:06 +0200

    bug and fruit

--- src/reagent_workshop.cljs
+++ src/reagent_workshop.cljs
@@ -12,6 +12,15 @@
 (def cell-size 20)
 
 
+; Helpers
+
+(defn pos-x [ci]
+  (* (rem ci field-size) cell-size))
+
+(defn pos-y [ci]
+  (* (quot ci field-size) cell-size))
+
+
 
 ; App state
 
@@ -22,35 +31,43 @@
 
 
 (defn spawn-new-fruit! []
-  (swap! app-state :fruit (rand-pos)))
+  (swap! app-state
+         assoc :fruit (rand-int num-cells))
+
+  (println (clj->js @app-state)))
 
 
 
-(defn tick []
-  )
-
-
-; Helpers
-
-(defn pos-x [ci]
-  (* (rem ci field-size) cell-size))
-
-(defn pos-y [ci]
-  (* (quot ci field-size) cell-size))
 
 
 ; Views
 
+(defn fruit [ci]
+  (let [m  (/ cell-size 2)
+        cx (+ (pos-x ci) m)
+        cy (+ (pos-y ci) m)]
+    [:circle
+      {:className "fruit"
+       :width     cell-size
+       :height    cell-size
+       :cx        cx
+       :cy        cy
+       :r         (dec m)}]))
+
+
 (defn bug [ci]
   (let [m  (/ cell-size 2)
-        r  (- m 3)]
+        cx (+ (pos-x ci) m)
+        cy (+ (pos-y ci) m)]
     [:circle
       {:className "bug"
        :width     cell-size
        :height    cell-size
-       :cx        (+ (pos-x ci) m)
-       :cy        (+ (pos-y ci) m)
-       :r         r}]))
+       :cx        cx
+       :cy        cy
+       :r         (- m 3)
+       ;:transform "scale(0.7,1)"
+      }]))
 
 
 (defn cell [ci & {:keys [has-bug has-fruit]}]
@@ -63,7 +80,9 @@
       :x         (pos-x ci)
       :y         (pos-y ci)} ]
 
-    (if has-bug (bug ci))])
+    (if has-bug (bug ci))
+
+    (if has-fruit (fruit ci)) ])
 
 
 (defn field [bug fruit]
@@ -73,7 +92,9 @@
 
    (for [ci (range (* field-size field-size))]
       ^{:key ci}
-       (cell ci :has-bug (= ci bug) :has-fruit (= ci fruit)))])
+       (cell ci
+             :has-bug (= ci bug)
+             :has-fruit (= ci fruit)))])
 
 
 (defn main-view []
@@ -84,5 +105,6 @@
 
 
 (defn ^:export run []
+  (spawn-new-fruit!)
   (reagent/render-component [main-view]
                               (.-body js/document)))
